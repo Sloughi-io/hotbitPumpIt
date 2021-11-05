5d777f43449fa9249a5765a0bc5a0e7be23643a0
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2021-11-05 05:10:32 +0100

    update cross

--- hotbitPupmIt.js
+++ hotbitPupmIt.js
@@ -1,95 +1,69 @@
-///1 core
-let fixedQauntity = 20.000
-let currentPrice = 0.0
+
+let intialFixedQauntity = 20.000
+
 let fees = 0.2
 
-async function getCurrentPrice(coin) {
-	const urlMarketPrice = "https://api.hotbit.io/api/v1/market.last?market="+coin+"/USDT";
+async function hotbitPumpIt({coin="BNB",fixedQauntity=intialFixedQauntity}) {
 
-	const marketPrice = await fetch(urlMarketPrice, {
-    		method: 'GET', //
-			mode:"no-cors",
-    		credentials: 'same-origin', //
-    		headers: {
-			//	'Access-Control-Allow-Origin':'*',
-			//		"Allow":"*",
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'user-agent' :'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
-			referer : 'https://www.hotbit.io',
-		coockie:document.cookie,
-	}
-  })
-const res =  await marketPrice.text() ;
-  //document.write(res)
-      const json = marketPrice === "" ? {} : JSON.parse(marketPrice);
-      console.log("==========ssss=",json);
-	   return json;
- 
+let res_currentPrice = await fetch( "https://api.hotbit.io/api/v1/market.last?market="+coin+"/USDT");
+    let {result:currentPrice=0.0} = await res_currentPrice.json();
 
-}
 
-async function hotbitPumpIt(intialData) {
-const coockie = document.cookie;
+/// Calculate the quantity of the token
+const quantityUSDT = fixedQauntity / ( 1.0005 + ( fees / 100) )
+const quanitityToken = quantityUSDT / currentPrice
 
-const {coin,side="BUY"} = intialData;
 
-//price=555&quantity=0.0536&market=BNB%2FUSDT&side=BUY&type=LIMIT&hide=false&use_discount=false
+//const numberDecimal = currentPrice.toString().split('.')[1] && currentPrice.toString().split('.')[1].length || 8; //default 8
+const numberDecimal = document.querySelectorAll('dd')[4].querySelectorAll('p')[1].innerText.split(".")[1].split(" ")[0].length;
+console.log("intial info =>",{quanitityToken,currentPrice})
+
+let intialData = {
+	price:currentPrice,
+	quantity:quanitityToken.toFixed(numberDecimal),
+	coin,
+	side:'BUY',
+};
+
+const {price,quantity,side="BUY"} = intialData;
+
+const body = `price=${price}&quantity=${quantity}&market=${coin}%2FUSDT&side=${side}&type=LIMIT&hide=false&use_discount=false`;
 
 	console.log("Start BOTT");
-	console.log('get current price ....0%');
+	console.log('==',body,'==',{numberDecimal,intialData},);
 	// GET current price
-	
-	 
- // const resCurrentPrice = await marketPrice.json(); 
- const resCurrentPrice = await getCurrentPrice(coin);
-	console.log("======>>>",resCurrentPrice)
-  const {price:currentPrice} = resCurrentPrice || [];
-
-  if(!currentPrice)return alert("not found price market :(")
-  console.log("Price ==> "+currentPrice);
-
-  const quantityUSDT = fixedQauntity / ( 1.0005 + ( fees / 100) )
-const quantity = quantityUSDT / currentPrice;
-
-	const body = `price=${currentPrice}&quantity=${quantity}&market=${coin}%2FUSDT&side=${side}&type=LIMIT&hide=false&use_discount=false`;
-console.log('BODY ==>',body,'==');
+	//currentPrice = parseFloat(document.title)
 	//
-    	
+    	const coockie = document.cookie;
 	const url = 'https://www.hotbit.io/v1/order/create?platform=web';
 
-	console.log("Send request wait 3la raz9ak 23%...");
-	
+	console.log("send request wait 3la raz9ak 23%...");
+
 	const response = await fetch(url, {
     		method: 'POST', //
     		credentials: 'same-origin', //
     		headers: {
 			'Content-Type': 'application/x-www-form-urlencoded',
 			'user-agent' :'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
-			referer : 'https://www.hotbit.io',
+			'referer' : 'https://www.hotbit.io',
 		coockie,
 	},
     body,
   });
-  return response.json(); 
+  return response.json();
 }
-////// start
 
-///  Get current price from dashboard
-///const currentPrice = parseFloat(document.querySelector('.last.price-up').innerText);
+ function checkOrder(){
+const fixedQauntity = prompt("Qauntity") || 20.00;
+     const coin = prompt("Coin").toUpperCase() || "BNB";
 
-/// Calculate the quantity of the token
-//const quantityUSDT = fixedQauntity / ( 1.0005 + ( fees / 100) )
-//const quantity = quantityUSDT / currentPrice
+	hotbitPumpIt({coin,fixedQauntity}).then(data => {
+			console.log("#Respons 100%..");
+			console.info(data);
+
+		})
 
 
-let intialData = {
-	//price:currentPrice,
-	//quantity,
-	coin:"BNB",
-	//side:'BUY',
-};
+}
 
-hotbitPumpIt(intialData).then(data => {
-console.log("#Respons Complet 100%..");
-    console.table(data); 
-});
+ setTimeout(checkOrder,5000);
