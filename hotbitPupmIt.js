c7f0d8adefd24b27441b91904c679501e978764f
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2021-11-05 05:47:53 +0100

    version 0.0.7b

--- hotbitPupmIt.js
+++ hotbitPupmIt.js
@@ -3,8 +3,20 @@ let intialFixedQauntity = 20.000
 
 let fees = 0.2
 
+/*// active for fetch all data
+fetch("https://www.hotbit.io/v1/public/market/status24h?platform=web", requestOptions)
+  .then(response => response.json())
+  .then((data=[]) => {
+	globalThis.allCoins = (data.Content && data.Content.result) || [];
+	console.log("Load all list Coins 10%....")
+	 setTimeout(checkOrder,5000);
+});
+*/
+  .catch(error => console.log('error', error));
+
 async function hotbitPumpIt({coin="BNB",fixedQauntity=intialFixedQauntity}) {
 
+	
 let res_currentPrice = await fetch( "https://api.hotbit.io/api/v1/market.last?market="+coin+"/USDT");
     let {result:currentPrice=0.0} = await res_currentPrice.json();
 
@@ -13,9 +25,11 @@ let res_currentPrice = await fetch( "https://api.hotbit.io/api/v1/market.last?ma
 const quantityUSDT = fixedQauntity / ( 1.0005 + ( fees / 100) )
 const quanitityToken = quantityUSDT / currentPrice
 
-
+//const numberDecimal = globalThis.allCoins[coin+"USDT"]
 //const numberDecimal = currentPrice.toString().split('.')[1] && currentPrice.toString().split('.')[1].length || 8; //default 8
 const numberDecimal = document.querySelectorAll('dd')[4].querySelectorAll('p')[1].innerText.split(".")[1].split(" ")[0].length;
+
+	
 console.log("intial info =>",{quanitityToken,currentPrice})
 
 let intialData = {
@@ -65,5 +79,5 @@ const fixedQauntity = prompt("Qauntity") || 20.00;
 
 
 }
+setTimeout(checkOrder,5000); // remove if active auto fetch all coins
 
- setTimeout(checkOrder,5000);
